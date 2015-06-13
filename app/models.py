a06d79261ecb79d28e7ebf2064247d86bc112f98
Manoj <manojgowdanssm@gmail.com>
2015-06-13 15:17:55 +0530

    Database has been updated

--- app/models.py
+++ app/models.py
@@ -2,11 +2,24 @@ from app import db
 from hashlib import md5
 
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(160), index=True,unique=True)
+    last_seen = db.Column(db.DateTime)
+    followed = db.relationship('User', 
+                               secondary=followers, 
+                               primaryjoin=(followers.c.follower_id == id), 
+                               secondaryjoin=(followers.c.followed_id == id), 
+                               backref=db.backref('followers', lazy='dynamic'), 
+                               lazy='dynamic')
 	
     def is_authenticated(self):
         return True
@@ -25,6 +38,18 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
+   
+    @staticmethod
+    def make_unique_nickname(nickname):
+        if User.query.filter_by(nickname=nickname).first() is None:
+            return nickname
+        version = 2
+        while True:
+            new_nickname = nickname + str(version)
+            if User.query.filter_by(nickname=new_nickname).first() is None:
+                break
+            version += 1
+        return new_nickname
 	
     def avatar(self, size):
 	 return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % (md5(self.email.encode('utf-8')).hexdigest(), size)
@@ -37,3 +62,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post %r>' % (self.body)
+
+
+
+
